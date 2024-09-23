cbe5150348118def5210640de154c2eb9c31edd7
Pascal Martineau <pascal@lewebsimple.ca>
2024-09-23 11:35:23 -0400

    chore: rename wp-boilerplate => wp-barebones

--- scripts/post-create.sh
+++ scripts/post-create.sh
@@ -7,7 +7,7 @@ GIT_URL="https://gitlab.ledevsimple.ca/wp-sites/${PROJECT}.git"
 GIT_ORIGIN="ssh://git@gitlab.ledevsimple.ca:222/wp-sites/${PROJECT}.git"
 
 # Rename project
-sed -i -e "s/wp-boilerplate/${PROJECT}/g" .cpanel.yml .gitignore composer.json
+sed -i -e "s/wp-barebones/${PROJECT}/g" .cpanel.yml .gitignore composer.json
 
 # Initialize git repository
 git init
@@ -16,6 +16,8 @@ git fetch gitlab || error_code=$?
 if [ "${error_code-0}" -eq 0 ]; then
   git checkout main -f
   rm -rf wp-content/mu-plugins/*
+  rm -rf wp-content/dev-plugins/*
+  rm -rf wp-content/prod-plugins/*
   rm -rf wp-content/plugins/*
   rm -rf wp-content/themes/*
   git reset --hard
@@ -23,7 +25,7 @@ if [ "${error_code-0}" -eq 0 ]; then
 else
   git remote remove gitlab
   git add -A
-  git commit -am 'chore: initial wp-boilerplate project'
+  git commit -am 'chore: initial wp-barebones project'
 fi
 
 # Create database
