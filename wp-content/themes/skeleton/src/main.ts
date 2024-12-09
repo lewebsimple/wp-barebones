213d0bddeaa1a715bfae7b11c7353559f3e824f4
Pascal Martineau <pascal@lewebsimple.ca>
2024-12-09 13:54:12 -0500

    feat: Initial Vue setup

--- wp-content/themes/skeleton/src/main.ts
+++ wp-content/themes/skeleton/src/main.ts
@@ -1,4 +1,19 @@
 import "@/css/index.css";
+import { createApp, h } from "vue";
 import { version } from "../package.json";
+import App from "@/app.vue";
 
 console.log(`Skeleton v${version} (${import.meta.env.MODE})`);
+
+// Create Vue.js application from #app root element
+const appElement = document.getElementById("app");
+const innerHTML = appElement?.innerHTML || "";
+export const app = createApp({
+  render() {
+    return h(App, null, {
+      default: () => h("div", { innerHTML }),
+    });
+  },
+});
+
+app.mount("#app", true);
